151d3c7783e6883538d8c981f79fc701c7722fb1
Gian Carlo Pace <giancarlo.pace@hyland.com>
2023-03-21 23:51:00 +0100

    initial commit

---
+++ src/main/java/it/xpug/kata/birthday/greetings/Main.java
@@ -0,0 +1,15 @@
+package it.xpug.kata.birthday.greetings;
+
+import java.io.IOException;
+import java.text.ParseException;
+import javax.mail.MessagingException;
+
+public class Main {
+
+    public static void main(String[] args)
+        throws IOException, ParseException, MessagingException {
+        BirthdayService service = new BirthdayService();
+        service.sendGreetings("employee_data.txt", new XDate(), "localhost", 25);
+    }
+
+}
---
+++ src/test/java/it/xpug/kata/birthday/greetings/EmployeeTest.java
@@ -0,0 +1,31 @@
+package it.xpug.kata.birthday.greetings;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+public class EmployeeTest {
+
+    @Test
+    public void testBirthday() throws Exception {
+        Employee employee = new Employee("foo", "bar", "1990/01/31", "a@b.c");
+        assertFalse(employee.isBirthday(new XDate("2008/01/30")), "not his birthday");
+        assertTrue(employee.isBirthday(new XDate("2008/01/31")), "his birthday");
+    }
+
+    @Test
+    public void equality() throws Exception {
+        Employee base = new Employee("First", "Last", "1999/09/01", "first@last.com");
+        Employee same = new Employee("First", "Last", "1999/09/01", "first@last.com");
+        Employee different = new Employee("First", "Last", "1999/09/01", "boom@boom.com");
+
+        assertNotEquals(null, base);
+        assertNotEquals("", base);
+        assertEquals(base, same);
+        assertNotEquals(base, different);
+    }
+}

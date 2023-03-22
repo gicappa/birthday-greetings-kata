37262332b611e38357e187d5bc00a8c5a41e8b62
Gian Carlo Pace <giancarlo.pace@hyland.com>
2023-03-22 10:21:12 +0100

    fix: missing a ! in the greetings
    As a warm-up to understand our code base I fixed the test.
    Fixing that test allowed me to start running the tests and
    knowing my repo is well configured.

--- src/main/java/it/xpug/kata/birthday/greetings/BirthdayService.java
+++ src/main/java/it/xpug/kata/birthday/greetings/BirthdayService.java
@@ -24,7 +24,7 @@ public class BirthdayService {
                 employeeData[3]);
             if (employee.isBirthday(xDate)) {
                 String recipient = employee.getEmail();
-                String body = "Happy Birthday, dear %NAME%".replace("%NAME%",
+                String body = "Happy Birthday, dear %NAME%!".replace("%NAME%",
                     employee.getFirstName());
                 String subject = "Happy Birthday!";
                 sendMessage(smtpHost, smtpPort, "sender@here.com", subject, body, recipient);

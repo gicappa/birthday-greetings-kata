9150e6bfae4151e1cc1ea4f220b838934b4c15d7
Gian Carlo Pace <giancarlo.pace@hyland.com>
2023-03-22 22:22:04 +0100

    refactor: BirthDate using local date
    The BrithDate now uses a LocalDate as a base type for storing the date.

--- src/main/java/it/xpug/kata/birthday/domain/BirthDate.java
+++ src/main/java/it/xpug/kata/birthday/domain/BirthDate.java
@@ -1,45 +1,32 @@
 package it.xpug.kata.birthday.domain;
 
-import static java.util.Calendar.DAY_OF_MONTH;
-import static java.util.Calendar.MONTH;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * The representation of a birthday in the domain
  */
-public record BirthDate(Date date) {
+public record BirthDate(LocalDate date) {
 
     public BirthDate() {
-        this(new Date());
+        this(LocalDate.now());
     }
 
     public static BirthDate of(String date) {
-        try {
-            return new BirthDate(new SimpleDateFormat("yyyy/MM/dd").parse(date));
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        return new BirthDate(LocalDate.parse(date, formatter));
     }
 
     public int getDay() {
-        return getPartOfDate(DAY_OF_MONTH);
+        return date.getDayOfMonth();
     }
 
     public int getMonth() {
-        return 1 + getPartOfDate(MONTH);
+        return date.getMonthValue();
     }
 
     public boolean isSameDay(BirthDate anotherDate) {
         return anotherDate.getDay() == this.getDay() && anotherDate.getMonth() == this.getMonth();
     }
 
-    private int getPartOfDate(int part) {
-        var calendar = new GregorianCalendar();
-        calendar.setTime(date);
-        return calendar.get(part);
-    }
 }
